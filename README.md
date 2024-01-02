02fb52ab5c4ddaf72f34237e50c205f9db3e9857
Nafis Affan <nafisaffan14@gmail.com>
2024-01-02 20:47:33 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# reg-image-uplod" 
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('image'), (req, res) => {
+    res.send('File uploaded successfully!');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
+
